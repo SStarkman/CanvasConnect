f086cdb9965d65f59ec3d13228af91438231ac65
almalinux Cloud User <student@labSDANZF.5xyb12hqwafubchsk0krzmscbf.bx.internal.cloudapp.net>
2025-05-01 23:08:46 +0000

    Join Button successfully creates a membership for that user and the specified group

--- routes/main.py
+++ routes/main.py
@@ -30,6 +30,56 @@ def submit_group():
     start_date_time = datetime.strptime(request.form['start-date-time'], '%Y-%m-%dT%H:%M')
     end_date_time = datetime.strptime(request.form['end-date-time'], '%Y-%m-%dT%H:%M')
 
+    user = get_user()
+
+    if user:
+        # Create and add to the database
+        group = Group(
+            title=group_title,
+            description=group_description,
+            tag=tag,
+            start_date_time=start_date_time,
+            end_date_time=end_date_time,
+            organizer_id= user.id
+        )
+        db.session.add(group)
+        db.session.commit()
+
+        return redirect(url_for('main.index'))  # Redirect to a page that lists the groups
+
+    else:
+        return "User not found", 404
+
+@main.route('/join-group', methods=['POST'])
+def join_group():
+    print("Join Group Clicked")
+    group_id = request.form.get('group_id')
+    user = get_user()
+    print("Group id", group_id, "user", user)
+    new_membership = Membership(
+        user_id = user.id,
+        group_id = group_id
+    )
+
+    print("new membership created")
+
+    db.session.add(new_membership)
+    db.session.commit()
+
+    print("Added to database")
+
+    return redirect(url_for('main.index'))  # Redirect to a page that lists the groups
+
+def add_user(user_email, user_name):
+    print(f"{user_email} not in user table. Adding user now.")
+    new_user = User(
+        name=user_name,
+        email=user_email
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+def get_user():
     user_email = session.get('user_email')
     user_name = session.get('user_name')
     if user_email:
@@ -37,34 +87,9 @@ def submit_group():
         user = User.query.filter(User.email==user_email).first()
         print("User", user)
         if not user:
-            print(f"{user_email} not in user table. Adding user now.")
-            new_user = User(
-                name=user_name,
-                email=user_email
-            )
-            db.session.add(new_user)
-            db.session.commit()
+            add_user(user_email, user_name)
 
         user = User.query.filter(User.email==user_email).first()
-        print("User", user)
-
-        if user:
-            # Create and add to the database
-            group = Group(
-                title=group_title,
-                description=group_description,
-                tag=tag,
-                start_date_time=start_date_time,
-                end_date_time=end_date_time,
-                organizer_id=user.id
-            )
-            db.session.add(group)
-            db.session.commit()
-
-            return redirect(url_for('main.index'))  # Redirect to a page that lists the groups
-
-        else:
-            return "User not found", 404
+        return user
     else:
         return redirect(url_for('auth.login'))
-
